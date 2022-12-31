2aecf31a2f0fa3a5d33ff390285334bab783e620
mrahal <rahalmichel@hotmail.fr>
2022-12-31 10:19:17 +0100

    add leaflet map

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var rdfstore = require('rdfstore');
+const SparqlClient = require('sparql-http-client')
+const endpointUrl = 'https://dbpedia.org/sparql/'
+const query = `
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    PREFIX dbp: <http://dbpedia.org/property/>
+    PREFIX dnr: <http://dbpedia.org/resource/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX georss: <http://www.georss.org/georss/>
 
+    select * where {
+      ?subject skos:broader dbc:Mausoleums_by_country .
+      ?maqam dct:subject ?subject .
+      ?maqam dbp:religiousAffiliation dbr:Islam .
+      ?maqam georss:point ?position
+    }`;
 
+const client = new SparqlClient({ endpointUrl });
+
+(async function main() {
+  const stream = await client.query.select(query);
+
+  stream.on('data', row => {
+    Object.entries(row).forEach(([key, value]) => {
+      console.log(`${key}: ${value.value} (${value.termType})`)
+    })
+  })
+
+  stream.on('error', err => {
+    console.error(err)
+  })
+
+})();
 
 app.get('/', (req, res) => {
-  rdfstore.create(function (err, store) {
-    if (err) {
-      res.send(err);
-    }
-    store.execute('LOAD <http://https://dbpedia.org/page/Category:Mausoleums_by_country> INTO GRAPH <mbc>', function () {
-  
-      store.setPrefix('skos', 'http://www.w3.org/2004/02/skos/core#');
-      store.setPrefix('dct', 'http://purl.org/dc/terms/');
-      store.setPrefix('dbp', 'http://dbpedia.org/property/');
-      store.setPrefix('dbr', 'http://dbpedia.org/resource/');
-  
-      store.execute('select * where {\
-        ?subject skos:broader <mbc> .\
-        ?maqam dct:subject ?subject .\
-        ?maqam dbp:religiousAffiliation dbr:Islam\
-        }',
-        function (err, results) {
-          if (err) {
-            res.send(err);
-          }
-  
-          results.forEach(function (triple) {
-            res.send(triple.object.valueOf());
-          });
-        });
-  
-    });
-  });
+
 })
 
 app.listen(port, () => {
